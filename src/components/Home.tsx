6522545dfe8c936e698123fa613a443708cf66b0
Ritesh Shahu Gavade <riteshgavade53@gmail.com>
2026-04-19 09:29:16 +0530

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -22,7 +22,7 @@ export default function Home({ onNavigate }: { onNavigate: (id: string) => void 
       </div>
 
       <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold leading-tight mb-4 tracking-tight">
-        <a href="/menu_tools.html" target="_blank">
+        <a href="Newtoolpdfimage/public/menu_tools.html" target="_blank">
   Orange File Convert
 </a><br/>
         <span className="bg-gradient-to-br from-indigo-400 via-cyan-400 to-pink-400 bg-clip-text text-transparent">
