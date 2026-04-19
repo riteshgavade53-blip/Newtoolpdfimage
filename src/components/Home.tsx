f955d955a667937ccb7ef86a5040a25bfc67b08d
Ritesh Shahu Gavade <riteshgavade53@gmail.com>
2026-04-19 09:22:58 +0530

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -22,9 +22,9 @@ export default function Home({ onNavigate }: { onNavigate: (id: string) => void 
       </div>
 
       <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold leading-tight mb-4 tracking-tight">
-        <a href="Newtoolpdfimage\menu_tools.html" target="_blank">
-  orange File Covert.
-</a><br />
+        <a href="/menu_tools.html" target="_blank">
+  Orange File Convert
+</a><br/>
         <span className="bg-gradient-to-br from-indigo-400 via-cyan-400 to-pink-400 bg-clip-text text-transparent">
           PDF Toolkit
         </span>
