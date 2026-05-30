3a21043aae27fbab1abefeaf72178010dfa712e9
Ritesh Shahu Gavade <riteshgavade53@gmail.com>
2026-05-30 12:29:42 +0530

    Update MultilanguageFileConvert.tsx

--- src/components/MultilanguageFileConvert.tsx
+++ src/components/MultilanguageFileConvert.tsx
@@ -2,7 +2,9 @@ import { useEffect } from "react";
 
 export default function TablePdfToExcel() {
   useEffect(() => {
-    window.location.replace("/pdf-to-excel/index.html");
+    window.location.replace("/Pdf To Excel/index.html");
+    // Ya agar encoded chahiye:
+    // window.location.replace("/Pdf%20To%20Excel/index.html");
   }, []);
 
   return <div>Loading...</div>;
